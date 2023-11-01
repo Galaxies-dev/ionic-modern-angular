7015e47ffc3b0740f2b65344a7191fbcd4c74a42
Simon Grimm <saimon@devdactic.com>
2023-11-01 12:20:43 +0100

    Change API key to env.

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
 import { ApiResult, MovieResult } from './interfaces';
+import { environment } from 'src/environments/environment';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'a97243d7813d31446f6c43284e6854d5';
+const API_KEY = environment.apiKey;
 
 @Injectable({
   providedIn: 'root',
@@ -14,15 +15,14 @@ export class MovieService {
   constructor(private http: HttpClient) {}
 
   getTopRatedMovies(page = 1): Observable<ApiResult> {
-    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
-    // .pipe(
-    //   delay(2000) // Simulate slow network
-    // );
+    return this.http
+      .get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`)
+      .pipe(
+        delay(2000) // Simulate slow network
+      );
   }
 
   getMovieDetails(id: string): Observable<MovieResult> {
-    console.log(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
-
     return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
   }
 }
