c760f2b4e226a47b9d1be6bef10b29bea403acc0
Simon Grimm <saimon@devdactic.com>
2023-11-20 15:15:42 +0100

    Move infinite loading to defer block.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
   webDir: 'www/browser',
-  server: {
-    url: 'http://192.168.2.105:8100',
-    androidScheme: 'https',
-  },
 };
 
 export default config;
